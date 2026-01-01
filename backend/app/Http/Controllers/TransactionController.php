77fef3da3fc51caaeff359bcb68b0d9fec2420fa
William Dias <smdwilliam89@gmail.com>
2026-01-01 14:39:34 +0000

    feat: create TransactionController and API route

---
+++ backend/app/Http/Controllers/TransactionController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Transaction;
+use Illuminate\Http\Request;
+
+class TransactionController extends Controller
+{
+    public function index()
+    {
+        $transactions = Transaction::orderBy('date', 'desc')->get();
+
+        return response()->json($transactions);
+    }
+}
