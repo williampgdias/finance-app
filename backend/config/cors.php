5a830ef15a876c2531709e9cff0753aa10966da3
William Dias <smdwilliam89@gmail.com>
2026-01-11 16:34:09 +0000

    fix: Fixing the link of allowed_origins.

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -21,7 +21,7 @@ return [
 
     'allowed_origins' => [
         'http://localhost:3000',
-        'https://finance-app-seven-khaki.vercel.app/',
+        'https://finance-app-seven-khaki.vercel.app',
     ],
 
     'allowed_origins_patterns' => [],
