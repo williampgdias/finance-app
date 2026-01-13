4aa13f3d776543f0c52f2c1b23672d95beae60e8
William Dias <smdwilliam89@gmail.com>
2026-01-13 18:36:33 +0000

    fix: run migrations on startup

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 set -e
 
+echo "Fixing Apache Modules..."
 a2dismod mpm_event 2>/dev/null || true
 a2dismod mpm_worker 2>/dev/null || true
 a2dismod mpm_prefork 2>/dev/null || true
-
 a2enmod mpm_prefork
 
+echo "Running Database Migrations..."
+php artisan migrate --force
+
+echo "Caching Configuration..."
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
+
+echo "Starting Apache..."
 exec apache2-foreground
